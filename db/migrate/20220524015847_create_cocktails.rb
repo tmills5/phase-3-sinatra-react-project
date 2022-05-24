27fccf5fa46347e0ed80bd0443a5d57399431c5d
Merle Tyson Mills <tysonmills5@gmail.com>
2022-05-24 12:47:25 -0500

    added migrations and models

--- db/migrate/20220524015847_create_cocktails.rb
+++ db/migrate/20220524015847_create_cocktails.rb
@@ -1,8 +1,11 @@
 class CreateCocktails < ActiveRecord::Migration[6.1]
   def change
     create_table :cocktails do |t|
-      t.string :name,
-      t.text :instructions,
+      t.string :name
+      t.text :instructions
+      t.string :image
+
       t.timestamps
+    end
   end
 end
--- db/migrate/20220524020242_create_cocktail_ingredients.rb
+++ db/migrate/20220524020242_create_cocktail_ingredients.rb
@@ -1,6 +1,12 @@
 class CreateCocktailIngredients < ActiveRecord::Migration[6.1]
   def change
     create_table :cocktail_ingredients do |t|
-      
+      t.string :measurement
+
+      t.integer :cocktail_id
+      t.integer :ingredient_id
+
+      t.timestamps
+    end
   end
 end
