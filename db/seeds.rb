9988866d8ad45f37960e51b2ab291505aa9b3303
Merle Tyson Mills <tysonmills5@gmail.com>
2022-05-25 12:58:17 -0500

    working on seeding, trouble with ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+ice = Ingredient.find(10)
+vodka = Ingredient.find(11)
+oj = Ingredient.find(12)
+
+test_1_screwdriver = Cocktail.create(name: "ScrewDriver", instructions: "Mix", image: "Test URL")
+
+CocktailIngredient.create(measurement: "glassful", cocktail: test_1_screwdriver, ingredient: ice)
+CocktailIngredient.create(measurement: "3oz", cocktail: test_1_screwdriver, ingredient: vodka)
+CocktailIngredient.create(measurement: "fill remaining", cocktail: test_1_screwdriver, ingredient: oj)
+
 
 
 puts "✅ Done seeding!"
